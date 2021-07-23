a6bd9979519638d6bd037081656a12bd09dae04c
Nikon <nkukar@inbox.ru>
2021-07-23 12:56:31 +0300

    Zadanie 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-document.querySelector('h1').innerHTML = 'Знакомство с JS';
+const numberOfFilm = prompt("Сколько фильмов вы посмотрели?", '10');
+
+personalMovieDB = {
+	count: numberOfFilm,
+	movies: {},
+	actors: {},
+	genres: [],
+	private: false,
+};
+
+const a = prompt ('Последний из просмотренных фильмов?'),
+		b = prompt ('На сколько его оцените?'),
+	 	c = prompt ('Последний из просмотренных фильмов?'),
+	 	d = prompt ('На сколько его оцените?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
+
